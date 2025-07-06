9668a5b7cd704695f29ed49300821431d78addc8
Tatiana Tripkovich <tripkovich.t@yandex.ru>
2025-07-06 14:45:19 +0300

    FelineTest nuclear tests

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -8,8 +8,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,26 +18,28 @@ public class FelineTest {
     @Test
     public void getKittensShouldReturnDefaultValueTest() {
         assertEquals(1, feline.getKittens());
-        verify(feline, times(1)).getKittens();
-    }
+        }
 
     @Test
     public void getKittensWithArgumentShouldReturnSameValueTest() {
         int count = feline.getKittens(3);
         assertEquals(3, count);
-        verify(feline).getKittens(anyInt());
-    }
+        }
 
     @Test
     public void getFamilyShouldReturnFelineFamilyTest() {
         assertEquals("Кошачьи", feline.getFamily());
-        verify(feline).getFamily();
-    }
+        }
 
     @Test
     public void eatMeatShouldReturnPredatorFoodTest() throws Exception {
         List<String> food = feline.eatMeat();
         assertEquals(List.of("Животные", "Птицы", "Рыба"), food);
-        verify(feline).getFood("Хищник");
-    }
+        }
+
+        @Test
+    public void felineEatMeatReturnsPredatorFoodTest() throws Exception {
+        feline.eatMeat();
+            verify(feline).getFood("Хищник");
+        }
 }
